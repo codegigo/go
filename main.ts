ba9b425af6e27baa1dea03b42af42a429a55025a
codegigo <caimunnoi@icloud.com>
2024-09-16 10:55:50 +0700

    ai len mo

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 
 //% color=#0031AF icon="\uf06e" 
 //% groups='["Basic", "Ball", "Face", "Card", "Color", "Tracking", "Learn"]'
+//% block="AILens"
 namespace AILens {
     const CameraAdd = 0X14;
     let DataBuff = pins.createBuffer(9);
